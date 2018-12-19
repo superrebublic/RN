70038e692ef434c6928653cf572de423c713abc2
YanZhang <superrepublic@163.com>
2018-12-19 11:04:33 +0800

    文本输入组件

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './less3';
+import App from './less5';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ AwesomeProject/less5.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
+
+//TextInput是一个允许用户输入文本的基础组件。
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40,marginTop:100}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
